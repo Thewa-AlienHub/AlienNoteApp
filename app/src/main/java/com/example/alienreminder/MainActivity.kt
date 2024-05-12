835168ed5671e3706c3ac90fd29fdaf0bcad813a
Thewa-AlienHub <117763807+Thewa-AlienHub@users.noreply.github.com>
2024-05-12 18:16:26 +0530

    setup all the view model and factory

--- app/src/main/java/com/example/alienreminder/MainActivity.kt
+++ app/src/main/java/com/example/alienreminder/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.alienreminder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.alienreminder.database.NoteDatabase
+import com.example.alienreminder.repository.NoteRepository
+import com.example.alienreminder.viewmodel.NoteViewModel
+import com.example.alienreminder.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         setupViewModel()
+
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application,noteRepository)
+        noteViewModel = ViewModelProvider(this,viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
